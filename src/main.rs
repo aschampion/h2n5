ea9789c198c9544771110a50dc8491edf44dbed9
Andrew Champion <andrew.champion@gmail.com>
2018-03-07 15:05:47 -0500

    Minimal tile rendering
    With @clbarnes.
    
    - Add JPEG encoding with parameters.
    - Add PNG encoding with parameters.
    - Fix coordinate parsing bug for multinumeral values.
    - Requires master n5 for now.

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,28 @@ extern crate actix_web;
 extern crate image;
 extern crate n5;
 extern crate ndarray;
+extern crate num_traits;
 extern crate regex;
 
 
+use std::io::{
+    Write,
+};
 use std::str::FromStr;
 
+use n5::{
+    DatasetAttributes,
+    DataType,
+    N5Reader,
+};
+use n5::filesystem::{
+    N5Filesystem,
+};
+
 use actix_web::*;
+
+
+const DEFAULT_TILE_BUFFER: usize = 2_000_000;
 
 
 #[derive(Debug, PartialEq)]
@@ -16,13 +32,44 @@ struct SlicingDims {
     channel_dim: Option<u32>,
 }
 
+#[derive(Debug, PartialEq)]
+struct JpegParameters {
+    quality: u8,
+}
+
+impl Default for JpegParameters {
+    fn default() -> JpegParameters {
+        JpegParameters {
+            quality: 100,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum EncodingFormat {
+    Jpeg(JpegParameters),
+    Png,
+}
+
+impl FromStr for EncodingFormat {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "jpg" | "jpeg" => Ok(EncodingFormat::Jpeg(JpegParameters::default())),
+            "png" => Ok(EncodingFormat::Png),
+            _ => Err(()),
+        }
+    }
+}
+
 #[derive(Debug)]
 struct TileSpec {
     n5_dataset: String,
     slicing_dims: SlicingDims,
     tile_size: [u32; 2],
     coordinates: Vec<u64>,
-    format: String,
+    format: EncodingFormat,
 }
 
 impl FromStr for TileSpec {
@@ -30,7 +77,7 @@ impl FromStr for TileSpec {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let re = regex::Regex::new(r"^(?P<dataset>.*)/(?P<slicing>\d+_\d+(_\d+)?)/(?P<tile_size>\d+_\d+)(?P<coords>(/\d)+)\.(?P<format>.+)$")
+        let re = regex::Regex::new(r"^(?P<dataset>.*)/(?P<slicing>\d+_\d+(_\d+)?)/(?P<tile_size>\d+_\d+)(?P<coords>(/\d+)+)\.(?P<format>.+)$")
             .expect("TODO: Regex invalid");
         let caps = re.captures(s).expect("TODO: Regex did not match");
 
@@ -53,20 +100,91 @@ impl FromStr for TileSpec {
             .map(|n| u64::from_str(n).expect("TODO3"))
             .collect();
 
-        Ok(TileSpec{
+        Ok(TileSpec {
             n5_dataset,
             slicing_dims,
             tile_size,
             coordinates,
-            format: caps.name("format").unwrap().as_str().into(),
+            format: EncodingFormat::from_str(caps.name("format").unwrap().as_str())?,
         })
     }
 }
 
 #[allow(unknown_lints)]
 #[allow(needless_pass_by_value)]
-fn tile(req: HttpRequest) -> String {
-    format!("{:?}", TileSpec::from_str(&req.match_info()["spec"]))
+fn tile(req: HttpRequest) -> Result<HttpResponse> {
+    let spec = TileSpec::from_str(&req.match_info()["spec"]).expect("TODO");
+
+    let n = N5Filesystem::open(".").expect("TODO: n5 failed");
+    let data_attrs = n.get_dataset_attributes(&spec.n5_dataset)
+        .expect("TODO: dataset does not exist");
+    let mut tile_buffer: Vec<u8> = Vec::with_capacity(DEFAULT_TILE_BUFFER);
+
+    match *data_attrs.get_data_type() {
+        DataType::UINT8 => read_and_encode::<u8, _, _>(&n, &data_attrs, &spec, &mut tile_buffer)
+            .expect("TODO: read failed"),
+        _ => (),
+    }
+    Ok(HttpResponse::Ok()
+        .content_type("image/jpeg")
+        .body(tile_buffer)
+        .unwrap())
+}
+
+// TODO: u8 only.
+// TODO: Single channel only.
+fn read_and_encode<T, N: N5Reader, W: Write>(
+    n: &N,
+    data_attrs: &DatasetAttributes,
+    spec: &TileSpec,
+    writer: &mut W,
+) -> Result<(), std::io::Error>
+where n5::VecDataBlock<T>: n5::ReadableDataBlock + n5::WriteableDataBlock,
+      DataType: n5::DataBlockCreator<std::vec::Vec<T>>,
+      T: Clone + num_traits::identities::Zero {
+
+    // Express the spec tile as an N-dim bounding box.
+    let mut size = vec![1i64; data_attrs.get_dimensions().len()];
+    size[spec.slicing_dims.plane_dims[0] as usize] = i64::from(spec.tile_size[0]);
+    size[spec.slicing_dims.plane_dims[1] as usize] = i64::from(spec.tile_size[1]);
+    let bbox = n5::BoundingBox::new(
+        spec.coordinates.iter().map(|n| *n as i64).collect(),
+        size,
+    );
+
+    // Read the N-dim slab of blocks containing the tile from N5.
+    let slab = n.read_ndarray::<T>(
+        &spec.n5_dataset,
+        data_attrs,
+        &bbox)?;
+    let data = slab.into_raw_vec();
+
+    // Get the image data as a byte slice.
+    let bytes: &[u8] = unsafe {
+        std::slice::from_raw_parts(
+            &data[..] as *const [T] as *const u8,
+            data.len() * std::mem::size_of::<T>())
+    };
+
+    match spec.format {
+        EncodingFormat::Jpeg(ref p) => {
+            let mut encoder = image::jpeg::JPEGEncoder::new_with_quality(writer, p.quality);
+            encoder.encode(
+                bytes,
+                spec.tile_size[0],
+                spec.tile_size[1],
+                image::ColorType::Gray(8 * std::mem::size_of::<T>() as u8)).expect("TODO: encoding");
+        },
+        EncodingFormat::Png => {
+            let mut encoder = image::png::PNGEncoder::new(writer);
+            encoder.encode(
+                bytes,
+                spec.tile_size[0],
+                spec.tile_size[1],
+                image::ColorType::Gray(8 * std::mem::size_of::<T>() as u8)).expect("TODO: encoding");
+        }
+    }
+    Ok(())
 }
 
 fn main() {
@@ -92,6 +210,6 @@ mod tests {
         });
         assert_eq!(ts.tile_size, [512u32, 512]);
         assert_eq!(ts.coordinates, vec![3u64, 2, 1]);
-        assert_eq!(&ts.format, "jpg");
+        assert_eq!(ts.format, EncodingFormat::Jpeg(JpegParameters::default()));
     }
 }
