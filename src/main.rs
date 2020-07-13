d176f6b1cfada401c160e239ac983beba0891cfc
Andrew Champion <andrew.champion@gmail.com>
2020-07-13 11:17:45 +0100

    Enable rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,33 @@
 use std::collections::HashMap;
-use std::io::{
-    Write,
-};
+use std::io::Write;
 use std::num::ParseIntError;
 use std::path::PathBuf;
 use std::str::FromStr;
 use std::sync::Arc;
 
-use actix_web::*;
 use actix_cors::Cors;
 use actix_web::web::Data;
 use actix_web::web::Query;
-use n5::{
-    DatasetAttributes,
-    DataType,
-    N5Reader,
-    ReinitDataBlock,
-    ReflectedType,
-    ReadableDataBlock,
-};
-use n5::filesystem::{
-    N5Filesystem,
-};
+use actix_web::*;
+use n5::filesystem::N5Filesystem;
 use n5::ndarray::prelude::*;
-use n5::smallvec::{SmallVec, smallvec};
+use n5::smallvec::{
+    smallvec,
+    SmallVec,
+};
+use n5::{
+    DataType,
+    DatasetAttributes,
+    N5Reader,
+    ReadableDataBlock,
+    ReflectedType,
+    ReinitDataBlock,
+};
 use structopt::StructOpt;
-
 
 mod cache;
 
 use cache::N5CacheReader;
-
 
 #[derive(Debug, PartialEq)]
 struct SlicingDims {
@@ -50,9 +47,7 @@ struct JpegParameters {
 
 impl Default for JpegParameters {
     fn default() -> JpegParameters {
-        JpegParameters {
-            quality: 100,
-        }
+        JpegParameters { quality: 100 }
     }
 }
 
@@ -81,19 +76,11 @@ impl EncodingFormat {
         match *self {
             EncodingFormat::Jpeg(ref p) => {
                 let mut encoder = image::jpeg::JPEGEncoder::new_with_quality(writer, p.quality);
-                encoder.encode(
-                    bytes,
-                    tile_size.w,
-                    tile_size.h,
-                    image_color_type)
-            },
+                encoder.encode(bytes, tile_size.w, tile_size.h, image_color_type)
+            }
             EncodingFormat::Png => {
                 let encoder = image::png::PNGEncoder::new(writer);
-                encoder.encode(
-                    bytes,
-                    tile_size.w,
-                    tile_size.h,
-                    image_color_type)
+                encoder.encode(bytes, tile_size.w, tile_size.h, image_color_type)
             }
         }
     }
@@ -159,7 +146,7 @@ impl Default for ChannelPacking {
 }
 
 impl FromStr for ChannelPacking {
-    type Err = ();  // TODO
+    type Err = (); // TODO
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s.to_lowercase().as_str() {
@@ -185,8 +172,7 @@ enum TileSpecError {
 
 impl ResponseError for TileSpecError {
     fn error_response(&self) -> HttpResponse {
-        HttpResponse::build(http::StatusCode::BAD_REQUEST)
-                .body(self.to_string())
+        HttpResponse::build(http::StatusCode::BAD_REQUEST).body(self.to_string())
     }
 }
 
@@ -205,7 +191,7 @@ impl FromStr for TileSize {
         let w = coords[0].parse::<u32>()?;
         let h = coords[1].parse::<u32>()?;
 
-        Ok(TileSize {w, h})
+        Ok(TileSize { w, h })
     }
 }
 
@@ -226,24 +212,21 @@ struct TileSpec {
 }
 
 impl FromStr for TileSpec {
-
     type Err = TileSpecError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let re = regex::Regex::new(concat!(
-                r"^(?P<dataset>.*)/(?P<slicing>\d+_\d+(_\d+)?)/",
-                r"(?P<tile_size>\d+_\d+)(?P<coords>(/\d+)+)\.(?P<format>.+)$"))
-            .expect("Impossible: regex is valid");
+            r"^(?P<dataset>.*)/(?P<slicing>\d+_\d+(_\d+)?)/",
+            r"(?P<tile_size>\d+_\d+)(?P<coords>(/\d+)+)\.(?P<format>.+)$"
+        ))
+        .expect("Impossible: regex is valid");
         let caps = re.captures(s).ok_or(TileSpecError::MalformedPath)?;
 
         let n5_dataset = caps["dataset"].into();
         let mut sd_vals = caps["slicing"].split('_').map(u32::from_str);
 
         let slicing_dims = SlicingDims {
-            plane_dims: [
-                sd_vals.next().unwrap()?,
-                sd_vals.next().unwrap()?,
-            ],
+            plane_dims: [sd_vals.next().unwrap()?, sd_vals.next().unwrap()?],
             channel_dim: sd_vals.next().transpose()?,
         };
 
@@ -254,7 +237,8 @@ impl FromStr for TileSpec {
             h: ts_vals.next().unwrap()?,
         };
 
-        let coordinates = caps["coords"].split('/')
+        let coordinates = caps["coords"]
+            .split('/')
             .filter(|n| !str::is_empty(*n))
             .map(u64::from_str)
             .collect::<Result<SmallVec<_>, _>>()?;
@@ -284,13 +268,12 @@ fn tile<N: N5Reader>(
     let spec = {
         let mut spec = TileSpec::from_str(&req.match_info()["spec"])?;
         spec.format.configure(&query);
-        spec.packing = ChannelPacking::from_query(&query)
-            .map_err(|_| TileSpecError::UnknownChannelPacking)?;
+        spec.packing =
+            ChannelPacking::from_query(&query).map_err(|_| TileSpecError::UnknownChannelPacking)?;
         spec
     };
 
-    if spec.tile_size.w > state.max_tile_size.w ||
-       spec.tile_size.h > state.max_tile_size.h {
+    if spec.tile_size.w > state.max_tile_size.w || spec.tile_size.h > state.max_tile_size.h {
         return Ok(HttpResponse::BadRequest()
             .reason("Maximum tile size exceeded")
             .finish());
@@ -300,22 +283,39 @@ fn tile<N: N5Reader>(
     let data_attrs = n.get_dataset_attributes(&spec.n5_dataset)?;
     // Allocate a buffer large enough for the uncompressed tile, as the
     // compressed size will be less with high probability.
-    let buffer_size = spec.tile_size.w as usize * spec.tile_size.h as usize
-        * spec.slicing_dims.channel_dim.map(|d| data_attrs.get_dimensions()[d as usize] as usize).unwrap_or(1)
+    let buffer_size = spec.tile_size.w as usize
+        * spec.tile_size.h as usize
+        * spec
+            .slicing_dims
+            .channel_dim
+            .map(|d| data_attrs.get_dimensions()[d as usize] as usize)
+            .unwrap_or(1)
         * data_attrs.get_data_type().size_of();
     let buffer_size = std::cmp::min(buffer_size, state.max_tile_prealloc);
     let mut tile_buffer: Vec<u8> = Vec::with_capacity(buffer_size);
 
     match *data_attrs.get_data_type() {
         DataType::UINT8 => read_and_encode::<u8, _, _>(&**n, &data_attrs, &spec, &mut tile_buffer)?,
-        DataType::UINT16 => read_and_encode::<u16, _, _>(&**n, &data_attrs, &spec, &mut tile_buffer)?,
-        DataType::UINT32 => read_and_encode::<u32, _, _>(&**n, &data_attrs, &spec, &mut tile_buffer)?,
-        DataType::UINT64 => read_and_encode::<u64, _, _>(&**n, &data_attrs, &spec, &mut tile_buffer)?,
-        DataType::FLOAT32 => read_and_encode::<f32, _, _>(&**n, &data_attrs, &spec, &mut tile_buffer)?,
-        DataType::FLOAT64 => read_and_encode::<f64, _, _>(&**n, &data_attrs, &spec, &mut tile_buffer)?,
-        _ => return Ok(HttpResponse::NotImplemented()
-            .reason("Data type does not have an image renderer implemented")
-            .finish()),
+        DataType::UINT16 => {
+            read_and_encode::<u16, _, _>(&**n, &data_attrs, &spec, &mut tile_buffer)?
+        }
+        DataType::UINT32 => {
+            read_and_encode::<u32, _, _>(&**n, &data_attrs, &spec, &mut tile_buffer)?
+        }
+        DataType::UINT64 => {
+            read_and_encode::<u64, _, _>(&**n, &data_attrs, &spec, &mut tile_buffer)?
+        }
+        DataType::FLOAT32 => {
+            read_and_encode::<f32, _, _>(&**n, &data_attrs, &spec, &mut tile_buffer)?
+        }
+        DataType::FLOAT64 => {
+            read_and_encode::<f64, _, _>(&**n, &data_attrs, &spec, &mut tile_buffer)?
+        }
+        _ => {
+            return Ok(HttpResponse::NotImplemented()
+                .reason("Data type does not have an image renderer implemented")
+                .finish())
+        }
     }
     Ok(HttpResponse::Ok()
         .content_type(spec.format.content_type())
@@ -334,8 +334,9 @@ impl ResponseError for EncodingError {
     fn error_response(&self) -> HttpResponse {
         match *self {
             Self::Io(ref e) => e.error_response(),
-            Self::Image(_) => HttpResponse::build(http::StatusCode::BAD_REQUEST)
-                    .body(self.to_string()),
+            Self::Image(_) => {
+                HttpResponse::build(http::StatusCode::BAD_REQUEST).body(self.to_string())
+            }
         }
     }
 }
@@ -347,9 +348,10 @@ fn read_and_encode<T, N: N5Reader, W: Write>(
     spec: &TileSpec,
     writer: &mut W,
 ) -> Result<(), EncodingError>
-where n5::VecDataBlock<T>: n5::DataBlock<T> + ReinitDataBlock<T> + ReadableDataBlock,
-      T: ReflectedType + num_traits::identities::Zero {
-
+where
+    n5::VecDataBlock<T>: n5::DataBlock<T> + ReinitDataBlock<T> + ReadableDataBlock,
+    T: ReflectedType + num_traits::identities::Zero,
+{
     // Express the spec tile as an N-dim bounding box.
     let mut size = smallvec![1u64; data_attrs.get_dimensions().len()];
     size[spec.slicing_dims.plane_dims[0] as usize] = u64::from(spec.tile_size.w);
@@ -360,17 +362,14 @@ where n5::VecDataBlock<T>: n5::DataBlock<T> + ReinitDataBlock<T> + ReadableDataB
     let bbox = BoundingBox::new(spec.coordinates.clone(), size);
 
     // Read the N-dim slab of blocks containing the tile from N5.
-    let slab = n.read_ndarray::<T>(
-        &spec.n5_dataset,
-        data_attrs,
-        &bbox)?;
+    let slab = n.read_ndarray::<T>(&spec.n5_dataset, data_attrs, &bbox)?;
 
     let image_color_type = match spec.slicing_dims.channel_dim {
         Some(_dim) => {
             // TODO: match RGB/RGBA based on dimensions of dim.
             // Permute slab so that channels dimension is at end.
             unimplemented!()
-        },
+        }
         None => {
             let bits_per_channel = 8 / spec.packing.num_channels() * std::mem::size_of::<T>() as u8;
             match bits_per_channel {
@@ -385,8 +384,13 @@ where n5::VecDataBlock<T>: n5::DataBlock<T> + ReinitDataBlock<T> + ReadableDataB
                     ChannelPacking::GrayA => image::ColorType::La16,
                     ChannelPacking::RGBA => image::ColorType::Rgba16,
                 },
-                _ => return Err(std::io::Error::new(std::io::ErrorKind::InvalidData,
-                    "Packed bits per channel must be 8 or 16").into())
+                _ => {
+                    return Err(std::io::Error::new(
+                        std::io::ErrorKind::InvalidData,
+                        "Packed bits per channel must be 8 or 16",
+                    )
+                    .into())
+                }
             }
         }
     };
@@ -401,7 +405,9 @@ where n5::VecDataBlock<T>: n5::DataBlock<T> + ReinitDataBlock<T> + ReadableDataB
     // Get the image data as a byte slice.
     let bytes: &[u8] = unsafe { as_u8_slice(&data) };
 
-    spec.format.encode(writer, bytes, spec.tile_size, image_color_type).map_err(Into::into)
+    spec.format
+        .encode(writer, bytes, spec.tile_size, image_color_type)
+        .map_err(Into::into)
 }
 
 // Get the byte slice of a vec slice in a wrapper function
@@ -409,7 +415,8 @@ where n5::VecDataBlock<T>: n5::DataBlock<T> + ReinitDataBlock<T> + ReadableDataB
 unsafe fn as_u8_slice<T>(s: &[T]) -> &[u8] {
     std::slice::from_raw_parts(
         s as *const [T] as *const u8,
-        s.len() * std::mem::size_of::<T>())
+        s.len() * std::mem::size_of::<T>(),
+    )
 }
 
 /// Serve N5 datasets over HTTP as tiled image stacks.
@@ -480,7 +487,11 @@ impl<N: N5Reader> Clone for AppState<N> {
 }
 
 mod actix_middleware_kludge {
-    use actix_service::{IntoTransform, Service, Transform};
+    use actix_service::{
+        IntoTransform,
+        Service,
+        Transform,
+    };
     use actix_web::middleware::Condition;
 
     // Kludge necessary to work around limitations of conditional
@@ -510,10 +521,10 @@ mod actix_middleware_kludge {
 }
 
 fn main() -> std::io::Result<()> {
-
     let opt = Options::from_args();
-    let root_path = opt.root_path.to_str()
-        .ok_or_else(|| std::io::Error::new(std::io::ErrorKind::InvalidData, "Paths must be UTF-8"))?;
+    let root_path = opt.root_path.to_str().ok_or_else(|| {
+        std::io::Error::new(std::io::ErrorKind::InvalidData, "Paths must be UTF-8")
+    })?;
     let n5 = N5Filesystem::open(root_path)?;
     let max_tile_prealloc = opt.max_tile_prealloc;
     let max_tile_size = opt.max_tile_size;
@@ -535,25 +546,26 @@ fn main() -> std::io::Result<()> {
     }
 }
 
-fn run_server<N: N5Reader + Send + Sync + 'static>(opt: Options, state: AppState<N>) -> std::io::Result<()> {
+fn run_server<N: N5Reader + Send + Sync + 'static>(
+    opt: Options,
+    state: AppState<N>,
+) -> std::io::Result<()> {
     let cors = opt.cors;
     env_logger::init();
 
-    let mut server = HttpServer::new(
-        move || {
-            use crate::actix_middleware_kludge::WrapCondition;
+    let mut server = HttpServer::new(move || {
+        use crate::actix_middleware_kludge::WrapCondition;
 
-            App::new()
-                .data(state.clone())
-                .wrap(actix_web::middleware::Logger::default())
-                .service(
-                    web::resource("/tile/{spec:.*}")
-                        .route(web::get().to(tile::<N>))
-                )
-                .wrap(WrapCondition::new(cors, Cors::new().send_wildcard()))
-        })
-        .bind((opt.bind_address, opt.port))?;
-    if let Some(threads) = opt.threads { server = server.workers(threads); }
+        App::new()
+            .data(state.clone())
+            .wrap(actix_web::middleware::Logger::default())
+            .service(web::resource("/tile/{spec:.*}").route(web::get().to(tile::<N>)))
+            .wrap(WrapCondition::new(cors, Cors::new().send_wildcard()))
+    })
+    .bind((opt.bind_address, opt.port))?;
+    if let Some(threads) = opt.threads {
+        server = server.workers(threads);
+    }
     server.run()
 }
 
@@ -566,11 +578,14 @@ mod tests {
         let ts = TileSpec::from_str("my_test/dataset/0_1/512_512/3/2/1.jpg").unwrap();
 
         assert_eq!(ts.n5_dataset, "my_test/dataset");
-        assert_eq!(ts.slicing_dims, SlicingDims{
-            plane_dims: [0u32, 1],
-            channel_dim: None,
-        });
-        assert_eq!(ts.tile_size, TileSize {w: 512, h: 512});
+        assert_eq!(
+            ts.slicing_dims,
+            SlicingDims {
+                plane_dims: [0u32, 1],
+                channel_dim: None,
+            }
+        );
+        assert_eq!(ts.tile_size, TileSize { w: 512, h: 512 });
         assert_eq!(ts.coordinates, SmallVec::from([3u64, 2, 1]));
         assert_eq!(ts.format, EncodingFormat::Jpeg(JpegParameters::default()));
     }
